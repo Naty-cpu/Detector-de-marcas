168619c35d1bb693915d8a5b416b6f4de84faf78
Natalia Souza <natalias18@outlook.com>
2020-01-13 13:51:12 -0200

    Atualização das P3

--- Algoritmo.py
+++ Algoritmo.py
@@ -1,27 +1,27 @@
-import cv
+import cv2
 import numpy as np
 
-img = cv.imread('Atividades_escaneadas (2)_page-0002.jpg',1) #Imagem de busca 
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img = cv2.imread("NM7510_P3_pacote_C_2_sem_2019_page-0005.jpg",1) 
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-template = cv.imread('template_direito.jpg',cv.IMREAD_GRAYSCALE)
+template = cv2.imread('template_direito.jpg',cv2.IMREAD_GRAYSCALE)
 w, h = template.shape[::-1]
 
-result = cv.matchTemplate(gray_img, template, cv.TM_CCOEFF_NORMED)
-loc = np.where(result >= 0.54)
+result = cv2.matchTemplate(gray_img, template, cv2.TM_CCOEFF_NORMED)
+loc = np.where(result >= 0.659)
 
 for pt in zip(*loc[::-1]):
-    cv.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (0, 255, 0), 3)
 
-template2 = cv.imread('template_esquerdo.jpg',cv.IMREAD_GRAYSCALE)
+template2 = cv2.imread('template_esquerdo.jpg',cv2.IMREAD_GRAYSCALE)
 r, f = template2.shape[::-1]
 
-result2 = cv.matchTemplate(gray_img, template2, cv.TM_CCOEFF_NORMED)
-loc2 = np.where(result2 >= 0.54)
+result2 = cv2.matchTemplate(gray_img, template2, cv2.TM_CCOEFF_NORMED)
+loc2 = np.where(result2 >= 0.659)
 
 for pt in zip(*loc2[::-1]):
-    cv.rectangle(img, pt, (pt[0] + r, pt[1] + f), (0, 255, 0), 3)
+    cv2.rectangle(img, pt, (pt[0] + r, pt[1] + f), (0, 255, 0), 3)
 
-img = cv.resize(img, (1080, 660))
-cv.imshow("img", img)
-cv.waitKey(0)
+img = cv2.resize(img, (1080, 660))
+cv2.imshow("img", img)
+cv2.waitKey(0)
